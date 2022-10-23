7a0fb2e5c3293ccfec1014b660109b1b027c682d
Hemanth R <hemanthrt18@gmail.com>
2022-10-23 14:28:53 +0530

    speakers

--- src/components/Speakers/SpeakerCard.js
+++ src/components/Speakers/SpeakerCard.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { FaLinkedinIn } from 'react-icons/fa'
 
 function SpeakerCard({ name, image, designation, linkedin }) {
   return (
     <div className='speakerCard'>
       <div className='speakerCard__photo'>
-        <img src={image} alt="" />
+        <div className='speakerCard__overlay' />
+        <img src={image} alt="" className='scp__img'/>
+        <div className='speakerCard__content'>
+          <a href={linkedin} target="_blank" rel="noreferrer" className='speaker_icon_div'>
+            <FaLinkedinIn className='speaker_icon'/>
+          </a>  
+        </div>
       </div>
       <h2>{name}</h2>
       <p>{designation}</p>
