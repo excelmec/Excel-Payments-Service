037eb0dee066e3ff16ca9129059f5c948cf78f4c
Anjana Sudevan <anjanasudevan38@gmail.com>
2022-10-28 18:27:30 +0530

    .

--- src/pages/ContactUsPage/ContactUsPage.js
+++ src/pages/ContactUsPage/ContactUsPage.js
@@ -1,13 +1,15 @@
 import React from 'react'
-// import { Contact } from '../../components'
+import { Contact } from '../../components'
+
+import './ContactUsPage.css'
 
 function ContactUsPage() {
   return (
     <div className='contact_us'>
-      <div className='page_header'>
+      {/* <div className='page_header'>
         <h1>Contact Us</h1>
-      </div>
-      {/* <Contact /> */}
+      </div> */}
+      <Contact />
     </div>
   )
 }
