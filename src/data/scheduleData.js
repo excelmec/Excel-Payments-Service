97b4bbabcb7cc3253ca0e1b17a9c88bfd7cf850c
Hemanth R <hemanthrt18@gmail.com>
2022-10-29 16:03:38 +0530

    Update scheduleData.js

--- src/data/scheduleData.js
+++ src/data/scheduleData.js
@@ -3,54 +3,54 @@ export const scheduleData = [
         id: 1,
         time: '10:00 AM - 10:30 AM',
         title: 'Inauguration',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+        description: 'The Chief Guest, Anas Rahman Junaid will be inaugurating the event. The inauguration will include an inaugural speech from Chairperson Excel, the Event Head and guest speakers.'
     },
     {
         id: 2,
         time: '10:30 AM - 11:15 AM',
         title: 'First Speaker Session',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+        description: 'Anas Junaid Rahman, Founder and MD, Hurun India'
     },
     {
         id: 3,
         time: '11:30 AM - 12:15 PM',
         title: 'Second Speaker Session',
-        description: 'adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+        description: 'Mukesh Bubna, Founder & CEO Monexo'
     },
     {
         id: 4,
         time: '12:30 PM - 1:15 PM',
         title: 'Third Speaker Session',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+        description: 'Lijil Lakshman, CA, Co-Founder and Managing Partner of LDS & Co. '
     },
     {
         id: 5,
         time: '1:15 PM - 1:45 PM',
-        title: 'Lunch',
+        title: 'Lunch Break',
         description: ''
     },
     {
         id: 6,
         time: '2:00 PM - 3:30 PM',
-        title: 'Panel Discussion',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+        title: 'Panel Discussion on the topic Cashless Society and Privacy',
+        description: 'The panel discussion will consist of the panelists sharing facts or personal experiences, express opinions, and answer audience questions.'
     },
     {
         id: 7,
         time: '3:30 PM - 4:00 PM',
-        title: 'Flash Talks & Interactive Session',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+        title: 'Flash Talks',
+        description: ''
     },
     {
         id: 8,
         time: '4:00 PM - 4:15 PM',
         title: 'Vote of Thanks',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+        description: 'Vote of thanks from the Chairperson and Organizers'
     },
     {
         id: 9,
         time: '4:15 PM - 5:00 PM',
         title: 'Ice breaker and Snacks',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+        description: ''
     },
 ]
