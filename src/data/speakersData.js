2784b5a6a2705621209fe6391c3884d095996c20
Anjana Sudevan <anjanasudevan38@gmail.com>
2022-10-29 18:45:47 +0530

    .

--- src/data/speakersData.js
+++ src/data/speakersData.js
@@ -1,6 +1,7 @@
 import LijilLakshman from "../assets/images/LijilLakshman.jpg";
 import MukeshBubna from "../assets/images/MukeshBubna.jpeg";
 import anas from "../assets/images/anas.jpeg";
+import kenz from "../assets/images/kenz.jpeg";
 
 export const speakersData = [
     {
@@ -12,13 +13,20 @@ export const speakersData = [
     },
     {
         id: 2,
+        name: 'Kenz Milliondots',
+        designation: 'Founder, Milliondots edu',
+        image: kenz,
+        linkedin: 'https://www.linkedin.com/in/kenzmilliondots'
+    },
+    {
+        id: 3,
         name: 'CA Lijil Lakshman',
         designation: 'Co-Founder and Managing Partner, LDS & Co',
         image: LijilLakshman,
         linkedin: 'https://www.linkedin.com/in/calijillakshman/'
     },
     {
-        id: 3,
+        id: 4,
         name: 'Mukesh Bubna',
         designation: 'Founder & CEO, Monexo',
         image: MukeshBubna,
